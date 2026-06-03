30e2a4f1a9ed2efb1fa5e89fea93196d87718edc
susmita <susmitaghosh.leelija@gmail.com>
2026-06-03 12:24:46 +0530

    changes

--- backend/identiqo/admin_api/urls.py
+++ backend/identiqo/admin_api/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('register/', views.admin_register, name='admin_register'),
-    path('login/', views.admin_login, name='admin_login'),
-    path('subscription/add/', views.add_subscription_plan, name='add_subscription_plan'),
-    path('subscriptions/', views.subscription_plan_list, name='subscription_plan_list'),
+    # path('register/', views.admin_register, name='admin_register'),
+    # path('login/', views.admin_login, name='admin_login'),
+    # path('subscription/add/', views.add_subscription_plan, name='add_subscription_plan'),
+    # path('subscriptions/', views.subscription_plan_list, name='subscription_plan_list'),
 ] 
